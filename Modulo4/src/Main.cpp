b8eef7319ed07c914e76870b0f6f807123bedd1d
Gustavo <gustavo_mariot@hotmail.com>
2018-05-23 10:53:18 -0300

    Projeto para o Modulo 4 criado

---
+++ Modulo4/src/Main.cpp
@@ -0,0 +1,20 @@
+/*
+ * Main.cpp
+ *
+ *  Created on: 23 de mai de 2018
+ *      Author: gustavo
+ */
+
+#include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+using namespace cv;
+using namespace std;
+
+int main(){
+	return 1;
+}
+
+
