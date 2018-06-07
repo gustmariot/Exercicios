f61e1b0afce86b2e603aa49f6415e7cc0c626458
gustavo_mariot <dgustavomoliveira@gmail.com>
2018-06-07 16:34:29 -0300

    Exercicio 1 pronto

--- Modulo4/src/Main.cpp
+++ Modulo4/src/Main.cpp
@@ -15,57 +15,53 @@ using namespace cv;
 using namespace std;
 
 int exercicio1(string entrada, string saida) {
-	Mat I = imread(entrada, CV_LOAD_IMAGE_GRAYSCALE);
-	if (I.empty())
+	Mat imgCinza = imread(entrada, CV_LOAD_IMAGE_GRAYSCALE);
+	if (imgCinza.empty()){
 		return -1;
+	}
 
-	Mat padded;                            //expand input image to optimal size
-	int m = getOptimalDFTSize(I.rows);
-	int n = getOptimalDFTSize(I.cols); // on the border add zero values
-	copyMakeBorder(I, padded, 0, m - I.rows, 0, n - I.cols, BORDER_CONSTANT,
+	Mat aux;
+	int linhas = getOptimalDFTSize(imgCinza.rows);
+	int colunas = getOptimalDFTSize(imgCinza.cols);
+
+	copyMakeBorder(imgCinza, aux, 0, linhas - imgCinza.rows, 0, colunas - imgCinza.cols, BORDER_CONSTANT,
 			Scalar::all(0));
 
-	Mat planes[] = { Mat_<float>(padded), Mat::zeros(padded.size(), CV_32F) };
-	Mat complexI;
-	merge(planes, 2, complexI);  // Add to the expanded another plane with zeros
+	Mat planos[] = { Mat_<float>(aux), Mat::zeros(aux.size(), CV_32F) };
+	Mat complexImgCinza;
+	merge(planos, 2, complexImgCinza);
 
-	dft(complexI, complexI); // this way the result may fit in the source matrix
+	dft(complexImgCinza, complexImgCinza);
 
-	// compute the magnitude and switch to logarithmic scale
-	// => log(1 + sqrt(Re(DFT(I))^2 + Im(DFT(I))^2))
-	split(complexI, planes);    // planes[0] = Re(DFT(I), planes[1] = Im(DFT(I))
-	magnitude(planes[0], planes[1], planes[0]);         // planes[0] = magnitude
-	Mat magI = planes[0];
+	split(complexImgCinza, planos);
+	magnitude(planos[0], planos[1], planos[0]);
+	Mat magI = planos[0];
 
-	magI += Scalar::all(1);                    // switch to logarithmic scale
+	magI += Scalar::all(1);
 	log(magI, magI);
 
-	// crop the spectrum, if it has an odd number of rows or columns
 	magI = magI(Rect(0, 0, magI.cols & -2, magI.rows & -2));
 
-	// rearrange the quadrants of Fourier image  so that the origin is at the image center
 	int cx = magI.cols / 2;
 	int cy = magI.rows / 2;
 
-	Mat q0(magI, Rect(0, 0, cx, cy));   // Top-Left - Create a ROI per quadrant
-	Mat q1(magI, Rect(cx, 0, cx, cy));  // Top-Right
-	Mat q2(magI, Rect(0, cy, cx, cy));  // Bottom-Left
-	Mat q3(magI, Rect(cx, cy, cx, cy)); // Bottom-Right
+	Mat q0(magI, Rect(0, 0, cx, cy));
+	Mat q1(magI, Rect(cx, 0, cx, cy));
+	Mat q2(magI, Rect(0, cy, cx, cy));
+	Mat q3(magI, Rect(cx, cy, cx, cy));
 
-	Mat tmp;                      // swap quadrants (Top-Left with Bottom-Right)
+	Mat tmp;
 	q0.copyTo(tmp);
 	q3.copyTo(q0);
 	tmp.copyTo(q3);
 
-	q1.copyTo(tmp);                // swap quadrant (Top-Right with Bottom-Left)
+	q1.copyTo(tmp);
 	q2.copyTo(q1);
 	tmp.copyTo(q2);
 
-	normalize(magI, magI, 0, 1, CV_MINMAX); // Transform the matrix with float values into a
-											// viewable image form (float between values 0 and 1).
-
-	imshow("Input Image", I);    // Show the result
-	imshow("spectrum magnitude", magI);
+	normalize(magI, magI, 0, 1, CV_MINMAX);
+	imshow("Input Image", imgCinza);
+	imshow("Resultado", magI);
 	waitKey();
 
 	return 0;
